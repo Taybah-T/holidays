becdb0ccab33dabf5db9e1cfdcf91e760052c9dc
Taybah Tahir <taybah.tahir@madetech.com>
2026-02-10 00:24:47 +0000

    submit button on holiday form works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -14,13 +14,7 @@ def holidays():
         
     return render_template("holiday.html", data=vacation)
 
-@app.post('/test')
-def createTest():
-    return "Success"
 
-@app.get("/holiday")
-def add_holiday():
-    return render_template("holiday.html")
 
 @app.get("/holiday/add")
 def add():
@@ -34,10 +28,15 @@ def post_holiday():
     end = request.form["end_date"]
     
     with open("holiday.json") as f:
-        adding = json.load(f)
+        holidays = json.load(f)
         
     holidays.append({"name":name, "start_date":start, "end_date":end})
+    
 
+    with open("holiday.json",'w') as f:
+        json.dump(holidays,f,indent=4)
+
+    return redirect(url_for("holiday"))
 
 if __name__ == "__main__":
     app.run(debug=True)
